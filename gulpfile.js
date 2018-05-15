a1f51e76600bb3b577e2b0597609208163f8d938
vgvineetgpt <vgvineetgpt@gmail.com>
2018-05-16 03:18:02 +0530

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const rename = require('gulp-rename');
+const runsequence = require('run-sequence');
+
+gulp.task('compressJS', () => {
+    return gulp.src(['public/javascripts/*.js', '!public/javascripts/*.min.js'])
+        .pipe(minify({
+            ext: {
+                src: '.js',
+                min: '.min.js'
+            }
+        }))
+        .pipe(gulp.dest('public/javascripts'));
+});
+
+gulp.task('compressCss', () => {
+    return gulp.src(['public/stylesheets/*.css',
+            '!public/stylesheets/*.min.css'
+        ])
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({
+            dirname: 'public/stylesheets',
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('compressThemeCss', () => {
+    return gulp.src(['views/themes/**/*.css',
+            '!views/themes/**/*.min.css'
+        ])
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest('views/themes/'));
+});
+
+// run the tasks
+gulp.task('deploy', (callback) => {
+    runsequence('compressJS', 'compressCss', 'compressThemeCss', callback);
+});
